55617a4ee4eb4c2cd57fe377ae318cb225e8c777
Lucas Yang <yangchenshin77@gmail.com>
2022-02-27 15:51:25 +0800

    Add Autocomplete example

--- examples/example-vue/src/App.vue
+++ examples/example-vue/src/App.vue
@@ -204,6 +204,67 @@
       </Listbox>
     </Block>
 
+    <Block title="Combobox (Autocomplete)" title-class="text-teal-400">
+      <Combobox v-model="selectedPerson">
+        <Float
+          placement="bottom"
+          :offset="4"
+          leave-active-class="transition ease-in duration-100"
+          leave-from-class="opacity-100"
+          leave-to-class="opacity-0"
+          @hide="query = ''"
+        >
+          <div class="relative w-64 text-left bg-white border border-gray-200 rounded-lg shadow-md cursor-default focus:outline-none sm:text-sm overflow-hidden">
+            <ComboboxInput
+              class="w-64 border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:outline-none focus:ring-0"
+              :display-value="(person) => person.name"
+              @change="query = $event.target.value"
+            />
+
+            <ComboboxButton class="absolute inset-y-0 right-0 flex items-center pr-2">
+              <HeroiconsOutlineSelector class="w-5 h-5 text-gray-400" aria-hidden="true" />
+            </ComboboxButton>
+          </div>
+
+          <ComboboxOptions class="absolute w-64 py-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            <div
+              v-if="filteredPeople.length === 0 && query !== ''"
+              class="relative py-2 px-4 text-gray-700 cursor-default select-none"
+            >
+              Nothing found.
+            </div>
+
+            <ComboboxOption
+              v-for="person in filteredPeople"
+              v-slot="{ selected, active }"
+              :key="person.id"
+              :value="person"
+              as="template"
+            >
+              <li
+                class="relative py-2 pl-10 pr-4 cursor-default select-none"
+                :class="active ? 'text-white bg-teal-600' : 'text-gray-900'"
+              >
+                <span
+                  class="block truncate"
+                  :class="{ 'font-medium': selected, 'font-normal': !selected }"
+                >
+                  {{ person.name }}
+                </span>
+                <span
+                  v-if="selected"
+                  class="absolute inset-y-0 left-0 flex items-center pl-3"
+                  :class="active ? 'text-white' : 'text-teal-600'"
+                >
+                  <HeroiconsOutlineCheck class="w-5 h-5" aria-hidden="true" />
+                </span>
+              </li>
+            </ComboboxOption>
+          </ComboboxOptions>
+        </Float>
+      </Combobox>
+    </Block>
+
     <Block title="Popover" title-class="text-rose-400">
       <Popover>
         <HighOrderFloat
@@ -233,11 +294,12 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import {
   Menu, MenuButton, MenuItems, MenuItem,
   Popover, PopoverButton, PopoverPanel,
   Listbox, ListboxButton, ListboxOptions, ListboxOption,
+  Combobox, ComboboxInput, ComboboxButton, ComboboxOptions, ComboboxOption,
 } from '@headlessui/vue'
 import { Float, FloatReference, FloatContent, FloatArrow } from 'headlessui-float-vue'
 import HighOrderFloat from '@/components/HighOrderFloat.vue'
@@ -255,4 +317,16 @@ const people = [
   { id: 6, name: 'Hellen Schmidt', unavailable: false },
 ]
 const selectedPerson = ref(people[0])
+const query = ref('')
+
+const filteredPeople = computed(() =>
+  query.value === ''
+    ? people
+    : people.filter((person) =>
+        person.name
+          .toLowerCase()
+          .replace(/\s+/g, '')
+          .includes(query.value.toLowerCase().replace(/\s+/g, ''))
+      )
+)
 </script>
