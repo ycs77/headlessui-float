a7738ef14c4b5614e4b0a6fb95057b05b19f0309
Lucas Yang <yangchenshin77@gmail.com>
2023-02-20 17:07:11 +0800

    Tweak testing files

--- packages/react/test/unit/components.test.tsx
+++ packages/react/test/unit/components.test.tsx
@@ -1,40 +1,17 @@
 import { Menu } from '@headlessui/react'
 import { Float } from '../../src/float'
-import { render, screen, userEvent, waitFor } from './utils'
+import { render, screen, userEvent, waitFor } from './utils/testing-library'
 
-describe('render components', () => {
+describe('Render components', () => {
   it('should to render <Float> with <Menu>', async () => {
     render(
       <Menu>
         <Float placement="bottom-start" offset={4}>
-          <Menu.Button className="flex justify-center items-center px-5 py-2 bg-indigo-50 hover:bg-indigo-100 text-indigo-500 text-sm rounded-md">
-            Options
-          </Menu.Button>
-
-          <Menu.Items className="w-48 bg-white border border-gray-200 rounded-md shadow-lg overflow-hidden focus:outline-none">
-            <Menu.Item>
-              {({ active }) => (
-                <button type="button" className={`block w-full px-4 py-1.5 text-left text-sm ${
-                  active ? 'bg-indigo-500 text-white' : ''
-                }`}>
-                  Account settings
-                </button>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <button type="button" className={`block w-full px-4 py-1.5 text-left text-sm ${
-                  active ? 'bg-indigo-500 text-white' : ''
-                }`}>
-                  Documentation
-                </button>
-              )}
-            </Menu.Item>
-            <Menu.Item disabled>
-              <span className="block w-full px-4 py-1.5 text-left text-sm opacity-50 cursor-default">
-                Invite a friend (coming soon!)
-              </span>
-            </Menu.Item>
+          <Menu.Button>Options</Menu.Button>
+          <Menu.Items>
+            <Menu.Item><button type="button">Account settings</button></Menu.Item>
+            <Menu.Item><button type="button">Documentation</button></Menu.Item>
+            <Menu.Item disabled><span>Invite a friend (coming soon!)</span></Menu.Item>
           </Menu.Items>
         </Float>
       </Menu>
@@ -54,8 +31,5 @@ describe('render components', () => {
     expect(screen.queryByRole('menu')).toBeInTheDocument()
     const menuItems = screen.queryAllByRole('menuitem')
     expect(menuItems).toHaveLength(3)
-    expect(menuItems[0]).toBeInTheDocument()
-    expect(menuItems[1]).toBeInTheDocument()
-    expect(menuItems[2]).toBeInTheDocument()
   })
 })
--- packages/vue/test/unit/components.test.ts
+++ packages/vue/test/unit/components.test.ts
@@ -1,35 +1,21 @@
 import { Transition, defineComponent } from 'vue'
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { Float } from '../../src/float'
-import { render, screen, userEvent } from './utils'
+import { render, screen, userEvent } from './utils/testing-library'
+import { html } from './utils/html'
 
-describe('render components', () => {
+describe('Render components', () => {
   it('should to render <Float> with <Menu>', async () => {
     render(defineComponent({
       components: { Menu, MenuButton, MenuItem, MenuItems, Float, Transition },
-      template: `
+      template: html`
         <Menu>
           <Float placement="bottom-start" :offset="4">
-            <MenuButton class="flex justify-center items-center px-5 py-2 bg-indigo-50 hover:bg-indigo-100 text-indigo-500 text-sm rounded-md">
-              Options
-            </MenuButton>
-
-            <MenuItems class="w-48 bg-white border border-gray-200 rounded-md shadow-lg overflow-hidden focus:outline-none">
-              <MenuItem v-slot="{ active }">
-                <button type="button" class="block w-full px-4 py-1.5 text-left text-sm" :class="{ 'bg-indigo-500 text-white': active }">
-                  Account settings
-                </button>
-              </MenuItem>
-              <MenuItem v-slot="{ active }">
-                <button type="button" class="block w-full px-4 py-1.5 text-left text-sm" :class="{ 'bg-indigo-500 text-white': active }">
-                  Documentation
-                </button>
-              </MenuItem>
-              <MenuItem disabled>
-                <span class="block w-full px-4 py-1.5 text-left text-sm opacity-50 cursor-default">
-                  Invite a friend (coming soon!)
-                </span>
-              </MenuItem>
+            <MenuButton>Options</MenuButton>
+            <MenuItems>
+              <MenuItem><button type="button">Account settings</button></MenuItem>
+              <MenuItem><button type="button">Documentation</button></MenuItem>
+              <MenuItem disabled><span>Invite a friend (coming soon!)</span></MenuItem>
             </MenuItems>
           </Float>
         </Menu>
@@ -45,8 +31,5 @@ describe('render components', () => {
     expect(screen.queryByRole('menu')).toBeInTheDocument()
     const menuItems = screen.queryAllByRole('menuitem')
     expect(menuItems).toHaveLength(3)
-    expect(menuItems[0]).toBeInTheDocument()
-    expect(menuItems[1]).toBeInTheDocument()
-    expect(menuItems[2]).toBeInTheDocument()
   })
 })
