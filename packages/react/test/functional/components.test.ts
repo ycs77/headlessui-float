8f0e3b2f62bc9ac1d9a7df8d3283a25f8a6baef0
Lucas Yang <yangchenshin77@gmail.com>
2023-02-28 02:19:17 +0800

    Fix functional popover test result is unstable

--- packages/react/test/functional/components.test.ts
+++ packages/react/test/functional/components.test.ts
@@ -1,21 +1,24 @@
 import { expect, test } from '@playwright/test'
-import { nextFrame, wait } from './utils/wait.js'
+import { wait } from './utils/wait.js'
 
 test('render floating menu', async ({ page }) => {
   await page.goto('http://localhost:3031/menu')
   await page.click('[data-testid=block-menu] button')
+  await wait(10)
   expect(await page.getByTestId('block-menu').screenshot()).toMatchSnapshot('menu.png')
 })
 
 test('render floating listbox', async ({ page }) => {
   await page.goto('http://localhost:3031/listbox')
   await page.click('[data-testid=block-listbox] button')
+  await wait(10)
   expect(await page.getByTestId('block-listbox').screenshot()).toMatchSnapshot('listbox.png')
 })
 
 test('render floating combobox', async ({ page }) => {
   await page.goto('http://localhost:3031/combobox')
   await page.click('[data-testid=block-combobox] button')
+  await wait(10)
   expect(await page.getByTestId('block-combobox').screenshot()).toMatchSnapshot('combobox.png')
 })
 
@@ -32,6 +35,6 @@ test('render floating dialog', async ({ page }) => {
 test('render floating popover', async ({ page }) => {
   await page.goto('http://localhost:3031/popover')
   await page.click('[data-testid=block-popover] button')
-  await nextFrame()
+  await wait(210)
   expect(await page.getByTestId('block-popover').screenshot()).toMatchSnapshot('popover.png')
 })
