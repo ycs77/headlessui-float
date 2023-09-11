12e95525109d9c5b61d3df4c9ec4428cbcbe6dbe
Richard Spenceley <richard.spenceley@zappistore.com>
2023-09-11 15:54:02 +0100

    add failing test to reproduce bug

--- packages/react/test/unit/components.test.tsx
+++ packages/react/test/unit/components.test.tsx
@@ -1,4 +1,4 @@
-import { Menu } from '@headlessui/react'
+import { Listbox, Menu } from '@headlessui/react'
 import { Float } from '../../src/float'
 import { render, screen, userEvent, waitFor } from './utils/testing-library'
 
@@ -35,5 +35,42 @@ describe('Render components', () => {
     await waitFor()
 
     expect(screen.queryByRole('menu')).toBeNull()
+  })
+  it('should to render <Float> with <Listbox>', async () => {
+    const Options = ({ options }: { options: string[] }) => (
+      <Listbox.Options>
+        {options.map(option => (
+          <Listbox.Option key={option} value={option}>
+            {option}
+          </Listbox.Option>
+        ))}
+      </Listbox.Options>
+    )
+    render(
+      <Listbox>
+        <Float placement="bottom-start" offset={4}>
+          <Listbox.Button>Options</Listbox.Button>
+          <Options options={['Account settings', 'Documentation', 'Invite a friend (coming soon!)']} />
+        </Float>
+      </Listbox>
+    )
+    await waitFor()
+
+    const button = screen.getByText('Options')
+    await waitFor()
+    expect(button).toBeInTheDocument()
+    expect(screen.queryByRole('listbox')).toBeNull()
+
+    await userEvent.click(button)
+    await waitFor()
+
+    expect(screen.queryByRole('listbox')).toHaveAttribute('data-headlessui-state', 'open')
+    const listboxOptions = screen.queryAllByRole('option')
+    expect(listboxOptions).toHaveLength(3)
+
+    await userEvent.click(button)
+    await waitFor()
+
+    expect(screen.queryByRole('listbox')).toBeNull()
   })
 })
