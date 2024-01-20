8f77f51a5021d9fbb7e55148b84744fb39142230
Lucas Yang <yangchenshin77@gmail.com>
2024-01-20 23:49:58 +0800

    (vue): Tweak arguments name

--- packages/vue/src/createHighOrderFloatComponent.ts
+++ packages/vue/src/createHighOrderFloatComponent.ts
@@ -1,11 +1,11 @@
 import { type SetupContext, h, mergeProps } from 'vue'
 import { Float, type FloatProps, type FloatSlotProps } from './float'
 
-export function createHighOrderFloatComponent(props: FloatProps) {
-  return ((userProps: FloatProps, { slots }: SetupContext) => {
+export function createHighOrderFloatComponent(baseProps: FloatProps) {
+  return ((overrideProps: FloatProps, { slots }: SetupContext) => {
     return h(Float, mergeProps(
-      props as Record<string, any>,
-      userProps as Record<string, any>
+      baseProps as Record<string, any>,
+      overrideProps as Record<string, any>
     ), slots)
   }) as unknown as {
     new (): {
