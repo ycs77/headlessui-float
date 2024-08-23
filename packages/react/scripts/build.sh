50de8fcea4c40c4df7a84512149d7a6fb87fc660
Lucas Yang <yangchenshin77@gmail.com>
2024-08-24 01:37:30 +0800

    Improve tsconfig

--- packages/react/scripts/build.sh
+++ packages/react/scripts/build.sh
@@ -4,9 +4,4 @@ rm dist -rf
 
 tsc --noEmit && \
 vite build && \
-tsc --declaration --emitDeclarationOnly --outDir ./dist
-
-rm dist/tsconfig.tsbuildinfo dist/*.d.ts
-rm dist/test -rf
-mv dist/src/* dist
-rm dist/src -rf
+tsc --declaration --emitDeclarationOnly -p tsconfig.build.json
--- packages/vue/scripts/build.sh
+++ packages/vue/scripts/build.sh
@@ -4,9 +4,4 @@ rm dist -rf
 
 vue-tsc --noEmit && \
 vite build && \
-vue-tsc --declaration --emitDeclarationOnly --outDir ./dist
-
-rm dist/tsconfig.tsbuildinfo dist/*.d.ts
-rm dist/test -rf
-mv dist/src/* dist
-rm dist/src -rf
+vue-tsc --declaration --emitDeclarationOnly -p tsconfig.build.json
